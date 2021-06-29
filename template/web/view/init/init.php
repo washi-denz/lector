37b83731c4a0fc836f8ac5f2ffdfb0d5300ee579
washi-denz <washidenz@gmail.com>
2021-06-29 11:22:08 -0500

    Modificando algunos estilos de la vista init

--- template/web/view/init/init.php
+++ template/web/view/init/init.php
@@ -131,9 +131,9 @@
 						--> 
 					</div>
 
-					<label class="text-yellow-600 font-medium text-lg block mb-2">Su nombre:</label>
+					<label class="text-yellow-600 font-medium text-lg mb-2">Su nombre:</label>
 					
-					<select class="focus:outline-none border-4 border-yellow-400 rounded-sm px-3 py-2 mx-auto mb-8 block" name="id_alumno">
+					<select class="focus:outline-none border-4 border-yellow-400 rounded-sm px-3 py-2 mx-auto mb-8 w-full md:w-auto block" name="id_alumno">
 						<option value="-Elija-">-Elija-</option>
 						<?php 
 							$alumnos = $fn->listaAlumnos($uniqid); 
